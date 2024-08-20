26969d0c2b1c15e84f8cac3c70daefadf5ab0e8d
Rupanzil Prince <rupanzilmamunprince@gmail.com>
2024-08-20 22:57:25 +0530

    first commit

---
+++ 01-basics/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello React!!</p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/rupanzil-mamun-prince/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Visit Me
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
