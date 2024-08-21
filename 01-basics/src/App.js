3c9f58c9efdcfe9b7fed1e22375c3dd836a316f3
Rupanzil Prince <rupanzilmamunprince@gmail.com>
2024-08-21 10:55:07 +0530

    simple example 01-basics

--- 01-basics/src/App.js
+++ 01-basics/src/App.js
@@ -1,8 +1,11 @@
+import Navbar from './Navbar'
+
 export default function App() {
   return (
     <>
       <div>
         <h1>This is the heading section</h1>
+        <Navbar />
       </div>
     </>
   )
---
+++ 01-basics/src/Navbar.js
@@ -0,0 +1,19 @@
+import './navbar.css'
+
+export default function Navbar() {
+  return (
+    <navbar>
+      <ul>
+        <a href="#">
+          <li>Home</li>
+        </a>
+        <a href="#">
+          <li>Contact</li>
+        </a>
+        <a href="#">
+          <li>Services</li>
+        </a>
+      </ul>
+    </navbar>
+  )
+}
